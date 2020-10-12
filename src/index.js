acf0e807f277e6c848e62c934797f5055f8084af
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-12 21:57:34 +0300

    del js

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import menuGaleryItems from './js/markup.js'
+
--- src/js/markup.js
+++ src/js/markup.js
@@ -4,5 +4,5 @@ import menuItems from '../menu.json'
 const menuGaleryItems = document.querySelector('.js-menu')
 const markupItemsTemplate = itemsTemplate(menuItems)
 
-menuGaleryItems.insertAdjacentHTML('beforeend', markupItemsTemplate)
+export default menuGaleryItems.insertAdjacentHTML('beforeend', markupItemsTemplate)
 console.log(itemsTemplate(menuItems))

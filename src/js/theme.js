cb1fac0beeacfb16bca9e4dae14754f6368094f6
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-14 16:53:55 +0300

    add theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,3 +1,20 @@
-const inputElThem = document.querySelector('#theme-switch-toggle')
-console.log(inputElThem.checked)
-console.dir(inputElThem.checked)
+const inputToggle = document.querySelector('#theme-switch-toggle')
+// console.log(inputToggle)
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+inputToggle.addEventListener('change', onInputToggleChecked)
+
+function onInputToggleChecked(event) {
+    if (event.target.checked) {
+        document.body.classList.add(Theme.DARK);
+        document.body.classList.remove(Theme.LIGHT)
+    } else {
+        document.body.classList.add(Theme.LIGHT);
+        document.body.classList.remove(Theme.DARK)    
+    }
+    return inputToggle;
+}

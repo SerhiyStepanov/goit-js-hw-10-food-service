826585139a19f5f9f22ef9caa4c4d82a4bd27c12
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-10-15 14:37:09 +0300

    localStorage

--- src/js/localthemesave.js
+++ src/js/localthemesave.js
@@ -1,8 +1,33 @@
 const inputChangeTheme = document.querySelector('#theme-switch-toggle')
-console.log(inputChangeTheme.checked)
 
 inputChangeTheme.addEventListener('change', onInputChecked)
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+getInputValue()
+
+// console.log(JSON.stringify(inputChangeTheme.checked))
+ 
 function onInputChecked(event) {
-console.log(event.target.checked)
+    const value = event.target.checked
+    if (value) {
+     localStorage.setItem('checked', JSON.stringify(value) )
+    }
+    else {
+        localStorage.removeItem('checked')
+    }
 }
+    
+function getInputValue() {
+    const saveValue = localStorage.getItem('checked')
+    if (saveValue) {
+        console.log(saveValue)
+        inputChangeTheme.checked = saveValue;
+        document.body.classList.add(Theme.DARK)
+    }
+    return JSON.parse(saveValue)
+}
+
